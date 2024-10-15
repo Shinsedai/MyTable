da83644f1a926a0db9f5a2fbe634cf5145e8cc02
Shinsedai <dylannj2002@gmail.com>
2024-10-15 16:55:43 +0200

    update repl.c

--- LangageC/CC1/class_db-main/src/repl.c
+++ LangageC/CC1/class_db-main/src/repl.c
@@ -61,39 +61,38 @@ void close_input_buffer(InputBuffer* input_buffer) {
 
 
 MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
-  if (strcmp(input_buffer->buffer, ".exit") == 0) {
-    close_input_buffer(input_buffer);
-    exit(EXIT_SUCCESS);
-  } else {
-    //TODO  here implement handling of other input as .exit
-    return META_COMMAND_UNRECOGNIZED_COMMAND;
-  }
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        close_input_buffer(input_buffer);
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
 }
 
 PrepareResult prepare_statement(InputBuffer* input_buffer,
                                 Statement* statement) {
-
-  if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-    statement->type = STATEMENT_INSERT;
-    return PREPARE_SUCCESS;
-  }
-  if (strcmp(input_buffer->buffer, "select") == 0) {
-    statement->type = STATEMENT_SELECT;
-    return PREPARE_SUCCESS;
-  }
-
-  return PREPARE_UNRECOGNIZED_STATEMENT;
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
 void execute_statement(Statement* statement) {
-  switch (statement->type) {
-    case (STATEMENT_INSERT):
-    //TODO Implement the command here
-      break;
-    case (STATEMENT_SELECT):
-      //TODO implement the command here 
-      break;
-  }
+    switch (statement->type) {
+        case (STATEMENT_INSERT):
+            // TODO Implement the command here
+            printf("Executed INSERT command.\n");
+            break;
+        case (STATEMENT_SELECT):
+            // TODO implement the command here 
+            printf("Executed SELECT command.\n");
+            break;
+    }
 }
 
 
