d8818604af39d8f9e3131676d9afdd03491157e5
Shinsedai <dylannj2002@gmail.com>
2024-10-16 10:52:13 +0200

    Update repl.c

--- LangageC/CC1/class_db-main/src/repl.c
+++ LangageC/CC1/class_db-main/src/repl.c
@@ -96,31 +96,31 @@ void execute_statement(Statement* statement) {
 }
 
 
-void repl(void){
-  InputBuffer* input_buffer = new_input_buffer();
-  while (true) {
-    print_prompt();
-    read_input(input_buffer);
-    if (input_buffer->buffer[0] == '.') {
-      switch (do_meta_command(input_buffer)) {
-        case (META_COMMAND_SUCCESS):
-          continue;
-        case (META_COMMAND_UNRECOGNIZED_COMMAND):
-          printf("Unrecognized command '%s'\n", input_buffer->buffer);
-          continue;
-      }
+void repl(void) {
+    InputBuffer* input_buffer = new_input_buffer();
+    while (true) {
+        print_prompt();
+        read_input(input_buffer);
+        if (input_buffer->buffer[0] == '.') {
+            switch (do_meta_command(input_buffer)) {
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                    continue;
+            }
+        }
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement)) {
+            case (PREPARE_SUCCESS):
+                printf("recognized statement\n");
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at start of '%s'.\n",
+                       input_buffer->buffer);
+                continue;
+        }
+        execute_statement(&statement);
+        printf("Executed.\n");
     }
-    Statement statement;
-    switch (prepare_statement(input_buffer, &statement)) {
-      case (PREPARE_SUCCESS):
-        printf("recognized statement\n");
-        break;
-      case (PREPARE_UNRECOGNIZED_STATEMENT):
-        printf("Unrecognized keyword at start of '%s'.\n",
-               input_buffer->buffer);
-        continue;
-    }
-     execute_statement(&statement);
-     printf("Executed.\n");
-  }
 }
