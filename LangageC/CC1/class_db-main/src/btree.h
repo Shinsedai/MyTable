87928a053707d9e584a12f2d83379cf904ff85d8
Shinsedai <dylannj2002@gmail.com>
2024-11-17 20:28:17 +0100

    update btree.h

--- LangageC/CC1/class_db-main/src/btree.h
+++ LangageC/CC1/class_db-main/src/btree.h
@@ -1,20 +1,23 @@
 #ifndef BTREE_H
 #define BTREE_H
 
+// Definition of the Row structure to store data
 typedef struct {
-    int id;
-    char name[32];
-    char email[256];
+    int id;               // Unique identifier for the row
+    char name[32];        // Name field
+    char email[256];      // Email field
 } Row;
 
+// Definition of the Node structure for the binary tree
 typedef struct Node {
-    Row* data;
-    struct Node* left;
-    struct Node* right;
+    Row* data;            // Pointer to the Row data
+    struct Node* left;    // Pointer to the left child
+    struct Node* right;   // Pointer to the right child
 } Node;
 
-Node* insert(Node* root, Row* data);
-Row* search(Node* root, int id);
-void free_tree(Node* root);
+// Function declarations for binary tree operations
+Node* insert(Node* root, Row* data); // Inserts data into the tree
+Row* search(Node* root, int id);     // Searches for a node by ID
+void free_tree(Node* root);          // Frees all memory used by the tree
 
 #endif
